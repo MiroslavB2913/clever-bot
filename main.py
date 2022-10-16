aa3167a6b6624b746112dffadccf54e801e51701
house in the world in <115923509+MiroslavB2913@users.noreply.github.com>
2022-10-16 10:46:45 +0000

    feat: Add Human class

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print ('hello World!')
+class Human:
+  def __init__(self):
+    self.name = 'none'
+    self.age = 0
+    self.gender = 'None'
+  def introduce(self):
+    print('Hello, my name is', self.name)
+  def add_info(self):
+    self.name = input('Name: ')
+    self.age = int(input('Age: '))
+    self.gender = input('gender:')
+
+obj = Human()
+obj.introduce()
+obj.add_info()
+obj.intoduce()
